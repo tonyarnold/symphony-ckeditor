076c1d5c2a83e051d36a507d97b4079b9ad484bb
Tony Arnold <tony@tonyarnold.com>
2009-12-08 23:14:37 +1100

    Adding an XML entity plugin.

--- assets/symphony.ckeditor.js
+++ assets/symphony.ckeditor.js
@@ -1,33 +1,30 @@
 jQuery(document).ready(function () {
-  
-    jQuery('textarea.ckeditor').each(function(index) {
-        var objectName = jQuery(this).attr('name');
-        jQuery(this).attr('id', objectName);
-        
-        CKEDITOR.replace( 
-            objectName,
-            {
-                height: this.offsetHeight,
-                removePlugins : 'font',
-                extraPlugins : 'uicolor',
-                uiColor : '#d6d6c7',
-                replaceByClassEnabled : false,
-                entities_processNumerical : true,
-                startupOutlineBlocks: true,
-                toolbar :
-                [
-                    ['Format'],
-                    ['Bold','Italic','Strike','-','Subscript','Superscript'],
-                    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-                    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-                    ['Link','Unlink','Anchor'],
-                    '/',
-                    ['Image','Table','HorizontalRule','SpecialChar'],
-                    ['PasteText','PasteFromWord','RemoveFormat'],
-                    ['Source','Maximize', 'ShowBlocks','-','About']
-                ]
-            }
-        );
-    });
+    var count = 0;
     
+    jQuery('textarea.ckeditor').each(function(index) {
+        var objectName = jQuery(this).attr('name');    
+        var configurationData = {
+            height : this.offsetHeight,
+            extraPlugins : 'uicolor,xmlentities',
+            removePlugins : 'font,entities',
+            uiColor : '#d6d6c7',
+            startupOutlineBlocks : true,
+            replaceByClassEnabled : false,
+            xmlentities : true,
+            toolbar : 
+            [
+                ['Format'],
+                ['Bold','Italic','Strike','-','Subscript','Superscript'],
+                ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+                ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+                ['Link','Unlink','Anchor'],
+                '/',
+                ['Image','Table','HorizontalRule','SpecialChar'],
+                ['PasteText','PasteFromWord','RemoveFormat'],
+                ['Source','Maximize', 'ShowBlocks','-','About']
+            ]
+        };
+        
+        CKEDITOR.replace(objectName, configurationData);
+    });
 });
